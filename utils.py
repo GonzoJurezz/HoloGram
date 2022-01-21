90cc62c32377668e68e76e0b8d7e6843990e0b3d
Lucas <lucas.breiner@gmx.de>
2022-01-21 22:16:55 +0100

    implement german horoscope

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_horoscope_by_day(zodiac_sign: int, day: str):
+def get_horoscope_by_day_eng(zodiac_sign: int, day: str):
     if "-" in day:
         day = day.replace("-", "")
         res = requests.get(
@@ -10,6 +10,15 @@ def get_horoscope_by_day(zodiac_sign: int, day: str):
     else:
         res = requests.get(
             f"https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-{day}.aspx?sign={zodiac_sign}")
+    soup = BeautifulSoup(res.content, 'html.parser')
+    data = soup.find('div', attrs={'class': 'main-horoscope'})
+    return data.p.text
+
+
+def get_horoscope_by_day_ger(zodiac_sign: str):
+    res = requests.get(
+        f"https://www.astrolantis.de/tageshoroskop-{zodiac_sign}.php")
+
     soup = BeautifulSoup(res.content, 'html.parser')
     data = soup.find('div', attrs={'class': 'main-horoscope'})
     return data.p.text
