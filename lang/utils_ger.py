aeaf193beceec8753e2af18d072b6b23c5a57cae
Lucas <lucas.breiner@gmx.de>
2022-01-23 21:19:20 +0100

    * form * reform

--- lang/utils_ger.py
+++ lang/utils_ger.py
@@ -1,20 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-class horoscopeOutput:
 
-    # horoskope: www.horoscope.com
-        def get_horoscope_by_day_eng(zodiac_sign: int, day: str):
-            if "-" in day:
-                day = day.replace("-", "")
-                res = requests.get(
-                    f"https://www.horoscope.com/us/horoscopes/general/horoscope-archive.aspx?sign={zodiac_sign}&laDate={day}")
-            else:
-                res = requests.get(
-                    f"https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-{day}.aspx?sign={zodiac_sign}")
-            soup = BeautifulSoup(res.content, 'html.parser')
-            data = soup.find('div', attrs={'class': 'main-horoscope'})
-            return data.p.text
+class horoscopeOutput:
 
     # horoskop: mein-horoskop-jeden-tag.com
         def get_horoscope_mhjt(zodiac_sign: str):
--- testing.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get(
-    f"https://www.mein-horoskop-jeden-tag.com/horoskop/heute/stier.htm")
-
-soup = BeautifulSoup(res.content, 'lxml')
-course_cards = soup.find('div', class_='txt')
-print(course_cards.p.text)
