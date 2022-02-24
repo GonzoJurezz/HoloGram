6c23e168eb64c36ef1b2a210a1e1c398694eab0b
Lucas <lucas.breiner@gmx.de>
2022-02-24 23:17:18 +0100

    * make gen_horoscope.py a class * adjust handlers * update requirements.txt

--- lang/gen_horoscope.py
+++ lang/gen_horoscope.py
@@ -1,14 +1,22 @@
 from transformers import pipeline
 import time
 
-horo = pipeline('text-generation',
-                model='./gpt2-horo',
-                tokenizer='dbmdz/german-gpt2',
-              )
 
-result = horo('Ihr Horoskop:', num_return_sequences=3, max_length=50, return_full_text=False)[0]['generated_text']
+def gen_horoskope():
 
-time_stamp = time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
-# result_file = open("generated_horoscopes", "a")
-# result_file.write(time_stamp + result)
-print(time_stamp + "\t" + result + "\n")
+
+    horo = pipeline('text-generation',
+                    model='./gpt2-horo',
+                    tokenizer='dbmdz/german-gpt2',
+                    )
+
+    result = horo('Dein Horoskop:', num_return_sequences=3, max_length=50, return_full_text=False)[0]['generated_text']
+
+    time_stamp = time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
+    # result_file = open("generated_horoscopes", "a")
+    # result_file.write(time_stamp + result)
+    print(time_stamp + "\t" + result + "\n")
+
+
+if __name__ == '__main__':
+    gen_horoskope()
