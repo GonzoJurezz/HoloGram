d4b90b294eca2e4bfee42bfcd73c2b75cee06eca
Lucas <lucas.breiner@gmx.de>
2022-03-02 22:22:37 +0100

    * fix typo * get output ready for bot

--- lang/gen_horoscope.py
+++ lang/gen_horoscope.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import time
 
 
-def gen_horoskope():
+def gen_horoscope():
 
 
     horo = pipeline('text-generation',
@@ -10,13 +10,15 @@ def gen_horoskope():
                     tokenizer='dbmdz/german-gpt2',
                     )
 
-    result = horo('Dein Horoskop:', num_return_sequences=3, max_length=50, return_full_text=False)[0]['generated_text']
+    horo.model.config.pad_token_id = horo.model.config.eos_token_id
+
+    result = horo('Ihr Horoskop für heute ist:', use_auth_token=True, max_length=50, truncation=True, return_full_text=False, )[0]['generated_text']
 
     time_stamp = time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
     # result_file = open("generated_horoscopes", "a")
     # result_file.write(time_stamp + result)
-    print(time_stamp + "\t" + result + "\n")
+    return time_stamp + "\t" + result + "\n"
 
 
 if __name__ == '__main__':
-    gen_horoskope()
+    gen_horoscope()
