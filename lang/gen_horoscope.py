9820809656d5b038720c1a437d2ec9101eb5f5ec
Lucas <lucas.breiner@gmx.de>
2022-05-27 21:50:50 +0200

    format generator

--- lang/gen_horoscope.py
+++ lang/gen_horoscope.py
@@ -4,7 +4,6 @@ import time
 
 def gen_horoscope():
 
-
     horo = pipeline('text-generation',
                     model='./gpt2-horo',
                     tokenizer='dbmdz/german-gpt2',
@@ -12,12 +11,11 @@ def gen_horoscope():
 
     horo.model.config.pad_token_id = horo.model.config.eos_token_id
 
-    result = horo('Ihr Horoskop für heute ist:', use_auth_token=True, max_length=50, truncation=True, return_full_text=False, )[0]['generated_text']
+    result = horo('Ihr Horoskop heute ist: ', use_auth_token=True, truncation=True, return_full_text=False, )[0]['generated_text']
 
     time_stamp = time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
-    # result_file = open("generated_horoscopes", "a")
-    # result_file.write(time_stamp + result)
-    return time_stamp + "\t" + result + "\n"
+
+    print(time_stamp + "\t" + result + "\n")
 
 
 if __name__ == '__main__':
